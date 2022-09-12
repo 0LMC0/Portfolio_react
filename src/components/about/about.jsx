a9b31a732d433524a519688093da6b7a9d98f2eb
Lautaro <lautycaceresss871@gmail.com>
2022-09-12 14:10:10 -0300

    last commit

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,14 @@
 // css import
 import "./about.css"
+// react-icons
+import { BiCodeBlock } from "react-icons/bi"
 
 const About = () => {
   return (
-    <section id="about"></section>
+    <section id="about">
+      <h5>Get to know me</h5>
+      <h2>About Me < BiCodeBlock /></h2>
+    </section>
   )
 }
 
--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,6 +1,6 @@
 // basic imports
 import "./nav.css"
-// import { useState } from "react";
+import { useState } from "react";
 // react-icons
 import { BiCodeAlt } from "react-icons/bi"
 import { ImHome } from "react-icons/im"
@@ -11,15 +11,15 @@ import { AiFillMessage } from "react-icons/ai"
 
 const Nav = () => {
 
-  // const [first, setfirst] = useState(second);+
+  const [ActiveNav, setActiveNav] = useState('#');
 
   return (
     <nav>
-      <a href="#contact" className="active">< ImHome /></a>
-      <a href="#about">< BiCodeAlt /></a>
-      <a href="#experience">< MdOutlineWork /></a>
-      <a href="#services">< MdMiscellaneousServices /></a>
-      <a href="#contact">< AiFillMessage /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={ActiveNav === '#' ? 'active' : '' }>< ImHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={ActiveNav === '#about' ? 'active' : '' }>< BiCodeAlt /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={ActiveNav === '#experience' ? 'active' : '' }>< MdOutlineWork /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={ActiveNav === '#services' ? 'active' : '' }>< MdMiscellaneousServices /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={ActiveNav === '#contact' ? 'active' : '' }>< AiFillMessage /></a>
     </nav>
   )
 }
